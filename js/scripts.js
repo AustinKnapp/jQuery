880e475387feadab9709bac6883d831a3572b76a
AustinKnapp and Brett Wille <austinknapp+brett@>
2016-06-14 17:09:57 -0700

    added jQuery content

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,36 @@ $(function() {
 
   });
 
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+    });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+    });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+    });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+    });
+
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("white-background");
+    $("body").addClass("custom1");
+    });
+
+  $("button#black").click(function() {
+    $("body").removeClass();
+    $("body").addClass("black-background");
+    $("body").addClass("custom2");
+    });
+
 });
